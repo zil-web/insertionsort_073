32840587237f01d01c3e7fde86b6692465dc4ef3
zil-web <126854462+zil-web@users.noreply.github.com>
2023-03-24 15:12:17 +0700

    memasukan data perulangan

--- project short/project short.cpp
+++ project short/project short.cpp
@@ -56,4 +56,8 @@ void display() {
 	cout << "Element Array yang telah tersusun" << endl;
 	cout << "=================================" << endl;
 
-	for (int j = 0; j <
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << " ";
+	}
+}
+	
