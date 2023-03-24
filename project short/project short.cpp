bb770d3cc24871ef804e59146e164d5bc501f6be
zil-web <126854462+zil-web@users.noreply.github.com>
2023-03-24 14:45:14 +0700

    Array yang anda masukkan maksimal 20 elemen

--- project short/project short.cpp
+++ project short/project short.cpp
@@ -5,11 +5,16 @@ int arr[20];
 int n;
 
 void input() {
-	while (true)
-	{
+	while (true){
 		cout << "masukan jumlah data pada array : ";
 		cin >> n;
 
-		if (n <= 20){}
+		if (n <= 20){
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+		}
 	}
 }
