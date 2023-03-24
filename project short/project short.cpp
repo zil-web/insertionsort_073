3905c0223b280b240b934af8417ab39bd6545f84
zil-web <126854462+zil-web@users.noreply.github.com>
2023-03-24 14:41:02 +0700

    menambahkan virtual int

---
+++ project short/project short.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "masukan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20){}
+	}
+}
